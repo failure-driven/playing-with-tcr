b607743463b8dec6777929e964258fd34aa11a63
Michael Milewski <saramic@gmail.com>
2019-08-26 14:57:47 +1000

    1566795467

--- tcr-rspec-dojo/lib/tennis.rb
+++ tcr-rspec-dojo/lib/tennis.rb
@@ -1,9 +1,13 @@
 class Tennis
+  def initialize
+    @points = { s: 0 }
+  end
+
   def score
-    @point == 1 ? "15 love" : "love all"
+    @points.values[0] == 1 ? "15 love" : "love all"
   end
 
   def point(player)
-    @point = 1
+    @points[player] += 1
   end
 end
