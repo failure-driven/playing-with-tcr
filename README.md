9129f8e70dc5c8e408ad95e6aed9b15857adc009
Michael Milewski <saramic@gmail.com>
2019-08-26 12:02:31 +1000

    start with mdx-deck
    mkdir docs/slides
    cd docs/slides
    npm init --yes
    npm install -D mdx-deck
    npm run build
    cp public/* ../

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Playing with TCR
+<h1 align="center">Playing with TCR</h1>
+
+<div align="center">
+
+[slides](https://failure-driven.github.io/playing-with-tcr/)
+
+</div>
 
 TCR = `(Test && Commit) || Revert`
 
---
+++ docs/slides/gatsby-config.js
@@ -0,0 +1,16 @@
+// gatsby-config.js
+module.exports = {
+  plugins: [
+    {
+      //resolve: 'gatsby-theme-mdx-deck',
+      options: {
+        // enable or disable gatsby-plugin-mdx
+        //mdx: false,
+        // source directory
+        //contentPath: 'decks',
+        // base path for routes generate by this theme
+        basePath: 'playing-with-tcr'
+      }
+    }
+  ]
+}
