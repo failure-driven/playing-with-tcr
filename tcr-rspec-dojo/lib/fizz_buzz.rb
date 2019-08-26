b8b8375edfeaeb2be65b8b8be8a818f1f8c54a16
Michael Milewski <saramic@gmail.com>
2019-08-26 14:48:40 +1000

    1566794920

--- tcr-rspec-dojo/lib/fizz_buzz.rb
+++ tcr-rspec-dojo/lib/fizz_buzz.rb
@@ -1,6 +1,8 @@
 class FizzBuzz
   def number(number)
     accumulator = []
+    accumulator << "fizz" if divisible_by(number, 3)
+    accumulator << "buzz" if divisible_by(number, 5)
     return "fizzbuzz" if divisible_by(number, 3) and divisible_by(number, 5)
     return "fizz" if divisible_by(number, 3)
     return "buzz" if divisible_by(number, 5)
