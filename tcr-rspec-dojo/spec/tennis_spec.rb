20ff7a72bd28378b8404525abd5e594ee0f46e38
Michael Milewski <saramic@gmail.com>
2019-08-26 15:14:41 +1000

    1566796481

--- tcr-rspec-dojo/spec/tennis_spec.rb
+++ tcr-rspec-dojo/spec/tennis_spec.rb
@@ -7,16 +7,12 @@ describe Tennis do
     { score: "love all", points: [] },
     { score: "15 love", points: [:s] },
     { score: "15 all", points: [:s, :r] },
+    { score: "30 all", points: [Array.new(2, :s), Array.new(2, :r)].flatten },
   ].each do |args|
     it "returns score #{args[:score].inspect} for points #{args[:points]}" do
       tennis.point(*args[:points])
       expect(tennis.score).to eq args[:score]
     end
-  end
-
-  it 'returns a score of "30 all" if server and reciever scores 2 points each' do
-    tennis.point(*[Array.new(2, :s), Array.new(2, :r)].flatten)
-    expect(tennis.score).to eq "30 all"
   end
 
   it 'returns a score of "deucde" if server and reciever scores 3 points each' do
