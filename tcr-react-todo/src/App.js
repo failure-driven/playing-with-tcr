492a43ec47555bb8a5bb1f60f41e744b646bf575
Michael Milewski <saramic@gmail.com>
2019-08-26 17:24:42 +1000

    1566804282

--- tcr-react-todo/src/App.js
+++ tcr-react-todo/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Form from './Form'
 
 function App() {
   return (
     <div>
       React app goes here!
+      <Form />
     </div>
   );
 }
--- tcr-react-todo/src/App.test.js
+++ tcr-react-todo/src/App.test.js
@@ -4,9 +4,11 @@ import App from "./App";
 
 it("renders", () => {
   const wrapper = shallow(<App />);
+  expect(wrapper.find("Form")).toHaveLength(1);
   expect(wrapper).toMatchInlineSnapshot(`
     <div>
       React app goes here!
+      <Form />
     </div>
   `);
 });
