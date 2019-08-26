5d2caac40cb34842250c6ab3c57eab9cd74d25ac
Michael Milewski <saramic@gmail.com>
2019-08-26 18:25:53 +1000

    1566807953

--- tcr-react-todo/src/List.jsx
+++ tcr-react-todo/src/List.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 
-const List = () => (
+const Item = ({item: {text}}) => <li>{text}</li>
+
+const List = ({items}) => (
   <ul>
+    {items.map(item => <Item key={item.id} item={item} />)}
   </ul>
 );
 
--- tcr-react-todo/src/List.test.jsx
+++ tcr-react-todo/src/List.test.jsx
@@ -4,8 +4,19 @@ import List from "./List";
 
 describe("WHEN rendered", () => {
   it("THEN renders", () => {
-    const wrapper = shallow(<List />);
+    const wrapper = shallow(<List items={[]} />);
     expect(wrapper.find("ul")).toHaveLength(1);
     expect(wrapper).toMatchInlineSnapshot(`<ul />`);
+  });
+
+  it("renders all the items", () => {
+    const items = [
+      {id: '123', text: 'todo 1'},
+      {id: 'ABC', text: 'todo 2'},
+    ]
+    const wrapper = shallow(<List items={items} />);
+    expect(wrapper.find("ul Item")).toHaveLength(2);
+    expect(wrapper.find("ul Item").at(0).prop("item")).toEqual({id: '123', text: 'todo 1'});
+    expect(wrapper.find("ul Item").at(1).prop("item")).toEqual({id: 'ABC', text: 'todo 2'});
   });
 });
