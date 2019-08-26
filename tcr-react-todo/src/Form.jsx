2d01236fd92733d5eb49d22d0d8057d108427b4e
Michael Milewski <saramic@gmail.com>
2019-08-26 17:22:09 +1000

    1566804129

--- tcr-react-todo/src/Form.jsx
+++ tcr-react-todo/src/Form.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react"
 
 const Form = () => {
-  const [value] = useState("")
-  return <input value={value}>
+  const [value, setValue] = useState("")
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value)
+  }
+
+  return <input value={value} onChange={onChangeInput}>
   </input>
 };
 
--- tcr-react-todo/src/Form.test.jsx
+++ tcr-react-todo/src/Form.test.jsx
@@ -9,8 +9,15 @@ describe("WHEN rendered", () => {
     expect(wrapper.find("input").prop("value")).toEqual("");
     expect(wrapper).toMatchInlineSnapshot(`
       <input
+        onChange={[Function]}
         value=""
       />
     `);
+  });
+
+  it("stores value that is typed into input", () => {
+    const wrapper = shallow(<Form />);
+    wrapper.find("input").simulate("change", { target: { value: "a todo" } });
+    expect(wrapper.find("input").prop("value")).toEqual("a todo");
   });
 });
