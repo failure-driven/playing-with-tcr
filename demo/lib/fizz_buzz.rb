6d5a568e15bc33c543da0f80065738862e8f096b
Michael Milewski <saramic@gmail.com>
2019-08-28 19:08:15 +1000

    fizz and buzz is all sorted

--- demo/lib/fizz_buzz.rb
+++ demo/lib/fizz_buzz.rb
@@ -2,4 +2,8 @@ class FizzBuzz
   def output
     "output here"
   end
+
+  def number(number_to_fizz_buzz)
+    number_to_fizz_buzz % 3 == 0 ? "fizz" : "buzz"
+  end
 end
--- demo/spec/fizz_buzz_spec.rb
+++ demo/spec/fizz_buzz_spec.rb
@@ -1,8 +1,22 @@
-require 'fizz_buzz'
+require "fizz_buzz"
 
 describe FizzBuzz do
-  it 'outputs fizz buzz' do
+  it "outputs fizz buzz" do
     fizz_buzz = FizzBuzz.new
     expect(fizz_buzz.output).to eq "output here"
+  end
+
+  it "outputs fizz for numbers divisible by 3" do
+    fizz_buzz = FizzBuzz.new
+    expect(fizz_buzz.number(3)).to eq "fizz"
+    expect(fizz_buzz.number(9)).to eq "fizz"
+    expect(fizz_buzz.number(12)).to eq "fizz"
+  end
+
+  it "outputs buzz for numbers divisible by 5" do
+    fizz_buzz = FizzBuzz.new
+    expect(fizz_buzz.number(20)).to eq "buzz"
+    expect(fizz_buzz.number(10)).to eq "buzz"
+    expect(fizz_buzz.number(25)).to eq "buzz"
   end
 end
