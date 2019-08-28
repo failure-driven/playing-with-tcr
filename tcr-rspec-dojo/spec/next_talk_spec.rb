1b395b51d3f1c53029e2a521553ac79beeb7dab3
Michael Milewski <saramic@gmail.com>
2019-08-29 05:05:28 +1000

    1567019128

--- tcr-rspec-dojo/spec/next_talk_spec.rb
+++ tcr-rspec-dojo/spec/next_talk_spec.rb
@@ -8,4 +8,11 @@ describe NextTalk do
       expect(next_talk.seconds_left).to eq 0
     end
   end
+
+  it "3600 seconds left an hour before" do
+    next_talk = NextTalk.new("Mon 2 Sep 2019 18:00:00")
+    Timecop.freeze(Time.local(2019, 9, 2, 17, 0, 0)) do
+      expect(next_talk.seconds_left).to eq 3600
+    end
+  end
 end
