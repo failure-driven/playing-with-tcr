d3408e02387c1ec04dcf1b2baafa106fa2c7fc02
Michael Milewski <saramic@gmail.com>
2019-08-29 04:58:12 +1000

    1567018692

--- tcr-rspec-dojo/lib/next_talk.rb
+++ tcr-rspec-dojo/lib/next_talk.rb
@@ -1,2 +1,8 @@
 class NextTalk
+  def initialize(talk_start_time)
+  end
+
+  def seconds_left
+    0
+  end
 end
--- tcr-rspec-dojo/spec/next_talk_spec.rb
+++ tcr-rspec-dojo/spec/next_talk_spec.rb
@@ -1,4 +1,11 @@
 require "next_talk"
+require "timecop"
 
 describe NextTalk do
+  it "calculates the time till next talk" do
+    next_talk = NextTalk.new("Mon 2 Sep 2019 18:00:00")
+    Timecop.freeze(Time.local(2019, 9, 2, 18, 0, 0)) do
+      expect(next_talk.seconds_left).to eq 0
+    end
+  end
 end
