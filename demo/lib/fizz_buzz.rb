3e8583618b7db709870a9fde334ca12271f366ee
Michael Milewski <saramic@gmail.com>
2019-08-28 19:13:01 +1000

    1566983581

--- demo/lib/fizz_buzz.rb
+++ demo/lib/fizz_buzz.rb
@@ -5,7 +5,7 @@ class FizzBuzz
 
   def number(number_to_fizz_buzz)
     return "fizz" if div_by(number_to_fizz_buzz, 3)
-    return "buzz" if number_to_fizz_buzz % 5 == 0
+    return "buzz" if div_by(number_to_fizz_buzz, 5)
 
     number_to_fizz_buzz
   end
