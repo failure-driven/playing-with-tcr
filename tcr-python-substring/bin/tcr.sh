1c37ab55db6e2349974b1247cd6a8aec87d01bab
Michael Milewski <saramic@gmail.com>
2019-09-02 08:11:50 +1000

    1567375910

--- tcr-python-substring/bin/tcr.sh
+++ tcr-python-substring/bin/tcr.sh
@@ -2,13 +2,18 @@
 
 TEST_OUTPUT=tmp/tcr_ouput.log
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
 test () {
   `python3 -m unittest discover fizzbuzz/test/` > $TEST_OUTPUT
 }
 
 commit () {
   cat $TEST_OUTPUT
-  echo "Green!!!"
+  echo "${GREEN}Green!!!${NC}"
   git add -A
   git commit -m "$(date +%s)"
   exit 0
