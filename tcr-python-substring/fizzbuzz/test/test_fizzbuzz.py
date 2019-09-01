38b677034437af7ac7b4e07b1af1aeb136d64fca
Michael Milewski <saramic@gmail.com>
2019-09-02 08:09:54 +1000

    1567375794

--- tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
@@ -6,17 +6,8 @@ sys.path.append(os.path.join(sys.path[0], '..'))
 import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_string_number_default(self):
-        self.assertEqual(fizzbuzz.number(2), "2")
-
-    def test_fizz_if_mod_3(self):
-        self.assertEqual(fizzbuzz.number(3), "fizz")
-
-    def test_fizz_if_mod_5(self):
-        self.assertEqual(fizzbuzz.number(5), "buzz")
-
-    def test_fizzbuzz_if_mod_3_and_5(self):
-        self.assertEqual(fizzbuzz.number(15), "fizzbuzz")
+    def test_one(self):
+        self.assertEqual(True, True)
 
 if __name__ == '__main__':
     unittest.main()
