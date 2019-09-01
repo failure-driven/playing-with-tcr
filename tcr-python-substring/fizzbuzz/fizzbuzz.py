e12c33ee150bda2aed895d81ad5f97042359225a
Michael Milewski <saramic@gmail.com>
2019-09-02 07:53:39 +1000

    got buzz working

--- tcr-python-substring/fizzbuzz/fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/fizzbuzz.py
@@ -1,6 +1,8 @@
 def number(number):
   if number % 3 == 0:
     return 'fizz'
+  if number % 5 == 0:
+    return 'buzz'
   return str(number)
 
 def output():
--- tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
@@ -6,8 +6,12 @@ sys.path.append(os.path.join(sys.path[0], '..'))
 import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_one(self):
-        assert True
+    def test_fizz_if_mod_3(self):
+        self.assertEqual(fizzbuzz.number(3), 'fizz')
+
+    def test_buzz_if_mod_5(self):
+        self.assertEqual(fizzbuzz.number(5), 'buzz')
+
 
 if __name__ == '__main__':
     unittest.main()
