f44f343afee9313deb75747f99991ead6e97a267
Michael Milewski <saramic@gmail.com>
2019-09-02 07:50:38 +1000

    1567374638

--- tcr-python-substring/fizzbuzz/fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/fizzbuzz.py
@@ -1,11 +1,2 @@
-def number(number):
-  if number % 3 == 0 and number % 5 == 0:
-    return 'fizzbuzz'
-  if number % 3 == 0:
-    return 'fizz'
-  if number % 5 == 0:
-    return 'buzz'
-  return str(number)
-
 def output():
-  return " ".join(map(number, list(range(1,100))))
+  return list(range(1,100))
