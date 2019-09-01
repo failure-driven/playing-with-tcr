6ce90914af1a285e3095e33cfbaf9755e98319ec
Michael Milewski <saramic@gmail.com>
2019-09-02 08:03:29 +1000

    reset for demo

--- tcr-python-substring/fizzbuzz/fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/fizzbuzz.py
@@ -1,11 +1,3 @@
-def number(number):
-  output = []
-  if number % 3 == 0:
-    output.append('fizz')
-  if number % 5 == 0:
-    output.append('buzz')
-
-  return str(number) if output == [] else "".join(output)
 
 def output():
-  return " ".join(map(number, list(range(1,100))))
+  return list(range(1,100))
--- tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
@@ -6,15 +6,8 @@ sys.path.append(os.path.join(sys.path[0], '..'))
 import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_fizz_if_mod_3(self):
-        self.assertEqual(fizzbuzz.number(3), 'fizz')
-
-    def test_buzz_if_mod_5(self):
-        self.assertEqual(fizzbuzz.number(5), 'buzz')
-
-    def test_fizzbuzz_if_mod_3_and_5(self):
-        self.assertEqual(fizzbuzz.number(15), 'fizzbuzz')
-        self.assertEqual(fizzbuzz.number(30), 'fizzbuzz')
+    def test_fizz(self):
+        self.assertEqual(True, True)
 
 if __name__ == '__main__':
     unittest.main()
