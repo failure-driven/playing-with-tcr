1f1250135ac52ae3438cbea52bc9776f94f5ceee
Michael Milewski <saramic@gmail.com>
2019-09-02 18:19:35 +1000

    1567412375

--- tcr-python-substring/fizzbuzz/fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/fizzbuzz.py
@@ -1,4 +1,6 @@
 def number(number):
+  if number % 5 == 0 and number % 3 == 0:
+    return 'fizzbuzz'
   if number % 5 == 0:
     return 'buzz'
   if number % 3 == 0:
--- tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
@@ -15,6 +15,9 @@ class TestFizzBuzz(unittest.TestCase):
     def test_buzz_if_mod_5(self):
         self.assertEqual(fizzbuzz.number(5), 'buzz')
 
+    def test_fizzbuzz_if_mod_5_and_3(self):
+        self.assertEqual(fizzbuzz.number(15), 'fizzbuzz')
+
 
 if __name__ == '__main__':
     unittest.main()
