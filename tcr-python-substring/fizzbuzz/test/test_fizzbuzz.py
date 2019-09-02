6c0d86f6e962d5fa001d879605ff483ce5690105
Michael Milewski <saramic@gmail.com>
2019-09-02 18:16:14 +1000

    py meetup is awesome

--- tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
+++ tcr-python-substring/fizzbuzz/test/test_fizzbuzz.py
@@ -6,8 +6,11 @@ sys.path.append(os.path.join(sys.path[0], '..'))
 import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_one(self):
-        self.assertEqual(True, True)
+    def test_default_to_the_number(self):
+        self.assertEqual(fizzbuzz.number(2), '2')
+
+    def test_fizz_if_mod_3(self):
+        self.assertEqual(fizzbuzz.number(3), 'fizz')
 
 if __name__ == '__main__':
     unittest.main()
